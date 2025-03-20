0702281b0d019fc9366bdac4f29a02613834c9b5
MINEDUCYT <mateoantoniomenjivarmelara@gmail.com>
2025-03-20 17:13:36 -0600

    HTML

--- Fetch/Ejercicio7.1.js
+++ Fetch/Ejercicio7.1.js
@@ -1,6 +1,10 @@
 fetch("https://jsonplaceholder.typicode.com/users")
     .then(response => response.json())
     .then(data => {
-        data.forEach(user => console.log(user.name));
+        const listaUSuarios = document.getElementById("js7.1");
+        data.forEach(user => {const li = document.createElement("li");
+            li.textContent = user.name;
+            listaUSuarios.appendChild(li);
+        });
     })
     .catch(error => console.error("Error al obtener datos:", error));
